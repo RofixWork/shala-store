e525ae1a067704b505f15c0134faa1c16da24a03
RofixWork <wourkout123@gmail.com>
2020-08-17 19:28:32 +0100

    Updates

---
+++ precache-manifest.7c5a22dea49cabb6fe38a5c8d0375297.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc32ad34174a03e806efd13434cf39aa",
+    "url": "/shala-store/index.html"
+  },
+  {
+    "revision": "9df5fe93d68ce77a0d04",
+    "url": "/shala-store/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ce5c0ee1a77bb76e1a04",
+    "url": "/shala-store/static/css/main.272a3839.chunk.css"
+  },
+  {
+    "revision": "9df5fe93d68ce77a0d04",
+    "url": "/shala-store/static/js/2.fcf3695d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shala-store/static/js/2.fcf3695d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5c0ee1a77bb76e1a04",
+    "url": "/shala-store/static/js/main.628e456e.chunk.js"
+  },
+  {
+    "revision": "4ef6d5182ab0012fda89",
+    "url": "/shala-store/static/js/runtime-main.63c7a1e1.js"
+  },
+  {
+    "revision": "8622e8b5e81a35ea5cd47f753b0aea32",
+    "url": "/shala-store/static/media/product-1.8622e8b5.jpg"
+  },
+  {
+    "revision": "2f8437da1542514f768ceeaba5548a82",
+    "url": "/shala-store/static/media/product-10.2f8437da.jpg"
+  },
+  {
+    "revision": "b3458bc051c250c4590d1550abcd630b",
+    "url": "/shala-store/static/media/product-11.b3458bc0.jpg"
+  },
+  {
+    "revision": "64dc2a541278c83e57a5d2380696c2ab",
+    "url": "/shala-store/static/media/product-12.64dc2a54.jpg"
+  },
+  {
+    "revision": "b05d17db6ac4c21342fd801f0e682788",
+    "url": "/shala-store/static/media/product-2.b05d17db.jpg"
+  },
+  {
+    "revision": "58a101d56f2efbe65d53f284c66132a7",
+    "url": "/shala-store/static/media/product-3.58a101d5.jpg"
+  },
+  {
+    "revision": "8dea815e1d72e990e435d6ad7f6093ac",
+    "url": "/shala-store/static/media/product-4.8dea815e.jpg"
+  },
+  {
+    "revision": "0511d63b221e0f7890cc83ef6e8dd4b2",
+    "url": "/shala-store/static/media/product-5.0511d63b.jpg"
+  },
+  {
+    "revision": "b2bae9ecdd0f62d32e2a7610577b5842",
+    "url": "/shala-store/static/media/product-6.b2bae9ec.jpg"
+  },
+  {
+    "revision": "4dfc84cc5aa8d117a02748b6b8b9efa7",
+    "url": "/shala-store/static/media/product-7.4dfc84cc.jpg"
+  },
+  {
+    "revision": "714fd6a7759ebdbb2c273a69c42b3b3a",
+    "url": "/shala-store/static/media/product-8.714fd6a7.jpg"
+  },
+  {
+    "revision": "4ae712e1d813cad1decc960be3b4a9f9",
+    "url": "/shala-store/static/media/product-9.4ae712e1.jpg"
+  }
+]);
